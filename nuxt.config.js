544916ac45a66d32bfc871ffe3ec75942dbd8f3f
plankalql <n.oberwoerder@gmail.com>
2021-06-22 11:24:23 +0200

    chore: update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: `${name} | ${description}`,
+    title: `${name} | Frontend & Design`,
     htmlAttrs: {
       lang: 'de',
     },
